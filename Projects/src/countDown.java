0ec656ba153cbf1b311b559d351b583b3a822b0c
Macharia Munene <machariapmunene@gmail.com>
2025-06-08 23:06:37 +0300

    For-Loops

---
+++ Projects/src/countDown.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class countDown {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many seconds to countdown from?: ");
+
+
+        int start = scanner.nextInt();
+        for (int i = start; i > 0; i--) {
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+
+        System.out.println("HAPPY NEW YEAR!!!!!!");
+
+    }
+}
--- Projects/src/numberGuessingGame.java
+++ Projects/src/numberGuessingGame.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+import java.util.Random;
+import java.util.Scanner;
 
 public class numberGuessingGame {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int guess;
+        int attempts = 0;
+        int min;
+        int max;
+
+        System.out.println("WELCOME TO THE NUMBER GUESSING GAME! ");
+
+        System.out.print("Enter the minimum number(First No.): ");
+        min = scanner.nextInt();
+
+        System.out.print("Enter the maximum number(Last No.): ");
+        max = scanner.nextInt();
+
+        int randomNumber = random.nextInt(min, max + 1);
+
+
+        do {
+            System.out.printf("Enter a number between %d and %d : ", min, max);
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < randomNumber) {
+                System.out.println("Too Low!: Try Again");
+            } else if (guess > randomNumber) {
+                System.out.println("Too High!: Try Again");
+            } else {
+                System.out.println("You have Won. The Number was " + randomNumber);
+                System.out.println("# of attempts: " + attempts);
+            }
+        } while (guess != randomNumber);
+
+//        System.out.println("You have Won");
+        scanner.close();
+
+
+    }
 }
