fa18e91bddc0047822ab87cdd22ece5aaafc3d2c
Macharia Munene <machariapmunene@gmail.com>
2025-06-29 22:50:26 +0300

    Wrapper-Classes

---
+++ Learning/src/Wrapper.java
@@ -0,0 +1,16 @@
+public class Wrapper {
+    public static void main(String[] args) {
+//        Wrapper Classes = Allow primitive values (int,char, double, boolean)
+//                          to  be used as objects. "Wrap them in an object"
+//                          Generally, don't wrap primitives unless you need an object.
+//                          Allows use of Collections Framework and static Utility Methods.
+
+
+//        Autoboxing
+        Integer a = 123;
+        Double b = 3.14;
+        Character c = 'c';
+        Boolean d = true;
+        
+    }
+}
