1c6d9ddd04c9a72a83e618ecfb634ea0ba72137f
Macharia Munene <machariapmunene@gmail.com>
2025-07-10 01:05:11 +0300

    Dynamic Arrays

---
+++ DSA/src/ArrayDynamic.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class ArrayDynamic {
+    public static void main(String[] args) {
+
+        DynamicArray dynamicArray = new DynamicArray(5);
+
+        dynamicArray.add("A");
+        dynamicArray.add("B");
+        dynamicArray.add("C");
+        dynamicArray.add("D");
+        dynamicArray.add("E");
+        dynamicArray.add("F");
+
+        dynamicArray.delete("A");
+        dynamicArray.delete("B");
+//        dynamicArray.delete("C");
+//        dynamicArray.delete("D");
+
+//        dynamicArray.insert(0, "X");
+//        dynamicArray.delete("A");
+//
+//
+//        System.out.println(dynamicArray.search("C"));
+        System.out.println(dynamicArray);
+        System.out.println("size: " + dynamicArray.size);
+        System.out.println("capacity: " + dynamicArray.capacity);
+        System.out.println("empty: " + dynamicArray.isEmpty());
+    }
+
+}
